f6cba0e8574440669912353c8412264db1be90cb
Nick Whelan <nickwhelan5@gmail.com>
2015-10-15 15:58:00 -0400

    Spawning a wall and other fun stuff
    base for game. for now

---
+++ Wall Assignment/Shooting(day2)/Assets/Scirpts/PickUp.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickUp : MonoBehaviour {
+    Rigidbody obj;
+    bool pickedup, canpickup;
+    short clicks=0;
+    void OnTriggerEnter(Collider col)
+    {
+            if ((col.gameObject.name == "Box(Clone)" || col.gameObject.name == "Box")&& clicks ==0)
+            {
+                //print("hit");
+                obj = col.gameObject.GetComponent<Rigidbody>();
+                canpickup = true;
+            }
+    }
+    void OnTriggerExit(Collider col)
+    {
+        if ((col.gameObject.name == "Box(Clone)"|| col.gameObject.name == "Box") && !pickedup)
+        {
+            //print("leave");
+            canpickup = false;
+        }
+    }
+    void Pickedup()
+    {
+        if (Input.GetKeyDown(KeyCode.E)&& obj !=null){
+            if (!pickedup)
+            {
+                obj.GetComponent<Rigidbody>().useGravity = false;
+                obj.GetComponent<Rigidbody>().freezeRotation = true;
+                //obj.GetComponent<BoxCollider>().enabled = false;
+                obj.transform.position = transform.position;
+                obj.transform.rotation = transform.rotation;
+                pickedup = true;
+                clicks = 1;
+            }
+            else if (pickedup)
+            {
+                obj.GetComponent<Rigidbody>().useGravity = true;
+               // obj.GetComponent<BoxCollider>().enabled = true;
+                obj.GetComponent<Rigidbody>().freezeRotation = false;
+                pickedup = false;
+                clicks = 0;
+                obj.velocity = transform.TransformDirection(new Vector3(0, 0, 10));
+            }
+        }
+    }
+    void Update() {
+        Pickedup();
+        if (pickedup)
+        {
+            obj.transform.position = transform.position;
+            obj.transform.rotation = transform.rotation;
+        }
+    }
+}
---
+++ Wall Assignment/Shooting(day2)/Assets/Scirpts/PlayerHud.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerHud : MonoBehaviour {
+    public Text heath;
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
